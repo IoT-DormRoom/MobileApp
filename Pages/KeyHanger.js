90753b5c190ad5b8bda25876ff831f7ebb5b84b2
Adeola Uthman <authman2@gmail.com>
2017-08-19 22:05:16 -0400

    Keyhook page

--- Pages/KeyHanger.js
+++ Pages/KeyHanger.js
@@ -3,7 +3,33 @@ import * as firebase from 'firebase';
 import { Dimensions, StyleSheet, View, Text, Button, Image, FlatList, WebView,
         TouchableHighlight, Alert, Modal, TextInput } from 'react-native';
 
+import superagent from 'superagent';
+
 import Page from './Page';
+
+
+
+// Methods to connect to the database.
+
+/**  Returns whether or not the keys is on the hooks.
+*   @param {Function} result The function that gets called when the data has been loaded.
+*   Returns two parameters, each stating whether the left and right keys are on their hooks.
+*   @param {Function} failure The block to run if there is ever an error loading the data.
+*/
+const keysOnHook = (result, failure) => {
+    superagent.get("https://dorm-service-server.herokuapp.com/keyhook", (err, resp) => {
+        if(err) { failure(err); return; }
+
+        var json = JSON.parse(resp.text);
+        result(json['leftHook'], json['rightHook']);
+    });
+};
+
+
+
+
+
+
 
 export default class KeyHanger extends Page {
 
@@ -14,12 +40,15 @@ export default class KeyHanger extends Page {
     constructor() {
         super();
         this.state = {
-            
+            keys: [
+                {name: "Left Key: ", value: false, key: 0},
+                {name: "Right Key: ", value: false, key: 1}
+            ]
         }
     }
 
     componentDidMount() {
-        
+        this.loadKeyHooks();
     }
 
     componentWillUnmount() {
@@ -37,11 +66,35 @@ export default class KeyHanger extends Page {
             <View style={styles.pageStyles}>
                 <Text style={styles.title}>Key Hanger</Text>
 
+                <FlatList ref={(FlatList) => {this._keysList = FlatList}}
+                        style={styles.list}
+                        data={this.state.keys} 
+                        renderItem={this.keyCell}/>
             </View>
         );
     }
 
 
+
+    /********************
+    *      METHODS      *
+    *********************/
+
+    // Loads the data for whether or not the room keys are on their respective hooks.
+    loadKeyHooks() {
+        keysOnHook( (leftRes, rightRes) => {
+            var arr = [];
+            arr.push({ name: "Left Key: ", value: leftRes, key: 0 });
+            arr.push({ name: "Right Key: ", value: rightRes, key: 1 });
+            
+            this.setState({ keys: arr });
+        }, (err) => {
+            Alert.alert('Error', '' + err, [{text:'Close',onPress:()=>{},style:'cancel'}],
+                {cancelable:true}
+            );
+            return;
+        });
+    }
 
 
 
@@ -49,6 +102,16 @@ export default class KeyHanger extends Page {
     /********************
     *       OTHER       *
     *********************/
+
+    keyCell = ({item}) => {
+        return (
+            <View style={styles.listCell}>
+                <Text style={styles.cellText}>
+                    {item.name} {item.value === true ? "On the hook" : "Not on the hook"}
+                </Text>
+            </View>
+        );
+    }
 
     
 }
@@ -69,5 +132,22 @@ const styles = StyleSheet.create({
         fontSize:40,
         textAlign:'center',
         fontWeight:'bold'
+    },
+
+
+    list: {
+        marginTop: 50
+    },
+    listCell: {
+        width: Dimensions.get('window').width,
+        height: 45,
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: 'white'
+    },
+    cellText: {
+        fontSize: 20,
+        fontFamily: 'Avenir',
+        textAlign: 'center'
     }
 });
